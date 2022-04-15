e7ab945046c01791c57019bf5a96e850def36594
Ramash3828 <ramash3828@gmail.com>
2022-04-15 08:36:18 +0600

    Hero-area and search box done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./Home.css";
+import bgImg from "../../images/bannerbackground.png";
+import { Button, Form, FormControl } from "react-bootstrap";
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home Page</h2>
+        <div
+            className="hero-area"
+            style={{
+                backgroundImage: `url(${bgImg})`,
+                height: `400px`,
+                objectFit: `cover`,
+                width: `100%`,
+            }}
+        >
+            <div className="container text-center">
+                <h2>Best Food for you. Order NOW!!!</h2>
+                <Form className="d-flex w-50 mx-auto">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className=""
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </div>
         </div>
     );
 };
